7af36c5830cfc8c34f719d615d6fd2d4d36b7619
ChristianLaraa <christian.lara.091204@gmail.com>
2023-03-28 00:59:29 -0600

    Agrego la clase Maquina y su driver

---
+++ DriverMaquinaCafe.java
@@ -0,0 +1,30 @@
+//Lara Martinez Christian Gael
+
+public class DriverMaquinaCafe {
+    public static void main(String[] args) {
+
+        MaquinaCafe Maquina = new MaquinaCafe();
+
+        while (Maquina.getVasos() > 0) {
+
+            Maquina.Americano();
+
+                             System.out.println();
+
+            Maquina.Expreso();
+
+                            System.out.println();
+
+            Maquina.Capuchino();
+
+                            System.out.println();
+        }
+
+        Maquina.MostrarInventario();
+
+                            System.out.println();
+
+        Maquina.TotalCafes();
+    }
+
+}
